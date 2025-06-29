a25b1df0aa72417e5df457300670331527a16579
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2025-06-29 13:34:46 +0000

    fix: Corriger la configuration de base dans vite.config.ts pour Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/", // Correction ici : base à la racine pour Netlify
   css: {
     postcss: {
       plugins: [tailwind()],
